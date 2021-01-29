41cafec646482d612af0864802715deb193ce916
BLDZ <69957760+BLDZ@users.noreply.github.com>
2021-01-29 09:58:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>BLDZ Main Page</title>
+</head>
+<body>
+    <h1>Heisann verden!</h1>
+    <p>Livet mitt er bare lidelse. Jeg trenger hjelp. Plis hjelp meg.</p>
+</body>
+</html>
